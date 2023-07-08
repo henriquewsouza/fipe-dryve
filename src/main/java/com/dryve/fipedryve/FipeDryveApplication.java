5c74767385840992d832da6c48d864b1dfb691ef
Henrique Waxman <henriquewsouza@gmail.com>
2023-07-08 16:33:46 -0300

    spring init

---
+++ src/main/java/com/dryve/fipedryve/FipeDryveApplication.java
@@ -0,0 +1,13 @@
+package com.dryve.fipedryve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeDryveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeDryveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dryve/fipedryve/FipeDryveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dryve.fipedryve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeDryveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
